a18dd2007d4702cd60a5bd9a404abc973734626e
Alicoder001 <coderkuchkarov@gmail.com>
2023-07-09 02:00:43 +0500

    added

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,15 @@
 import { Sequelize } from 'sequelize';
 const db = new Sequelize(
 	'postgres://countries_d301_user:EfdaZhILRps16W3BFj0FMHymxaaOeJ8u@dpg-ciks6hdgkuvinfl1a38g-a.singapore-postgres.render.com/countries_d301',
+	{
+		dialect: 'postgres',
+		protocol: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: 'true',
+			},
+		},
+	},
 );
 db.options.logging = false;
 try {
